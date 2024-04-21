aa2cf8c54e705a2b62e896a22b29e17d0ea8f71b
manavlade <manavlade14690@gmail.com>
2024-04-21 07:22:07 +0530

    Worked on middleware routes

--- auth-prac/middleware.ts
+++ auth-prac/middleware.ts
@@ -1,12 +1,42 @@
 import authConfig from "./auth.config"
 import NextAuth from "next-auth"
 
+import {
+    publicRoutes,
+    apiAuthPrefix,
+    authRoutes,
+    DEFAULT_LOGIN_REDIRECT
+} from "@/routes"
+
 const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
     // req.auth
-    const isLoggedIn = !!req.auth;
-    console.log("IS LOGGEDIN: ", isLoggedIn)
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth
+
+    const isAPIAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if (isAPIAuthRoute) {
+        return null;
+    }
+
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            return Response.redirect( new URL(DEFAULT_LOGIN_REDIRECT, nextUrl) )
+        }
+        return null
+    }
+
+
+    // Ye vale part mei error hai need to do some changes
+    // if(!isLoggedIn && !isPublicRoute){
+    //     return Response.redirect(new URL("/auth/login", nextUrl))
+    // }
+
+    // return null;
 })
 
 // Optionally, don't invoke Middleware on some paths
